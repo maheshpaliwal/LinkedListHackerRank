8afb82fc7b1b0212d3eea8c07bdce39451677539
MAHESH PALIWAL <39883849+maheshpaliwal@users.noreply.github.com>
2019-01-15 08:59:28 +0530

    Program for reversing a linked list

---
+++ Reverse a Linked List.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* reverse(SinglyLinkedListNode* head) {
+if(head->next==NULL){
+
+    return head;
+}
+else if(head->next->next==NULL){
+
+SinglyLinkedListNode* current=head->next;
+current->next=head;
+SinglyLinkedListNode* prev=head;
+prev->next=NULL;
+return current;
+
+}
+else{
+SinglyLinkedListNode* prev= head;
+SinglyLinkedListNode* current= prev->next;
+
+
+while(current->next!=NULL){
+
+SinglyLinkedListNode* temp=current->next;
+
+current->next=prev;
+prev=current;
+current=temp;
+
+
+
+
+}
+current->next=prev;
+head->next=NULL;
+return current;}
+}
+
+int main()
